d193459b39fddf884f072ece5de138eaa87ccea3
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-06-25 18:41:14 +0530

    Update route.ts

--- app/api/files/[projectId]/route.ts
+++ app/api/files/[projectId]/route.ts
@@ -4,9 +4,13 @@ import { PrismaClient } from '../../../generated/prisma'
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 
-export async function GET(_: Request, { params }: { params: { projectId: string } }) {
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const segments = url.pathname.split("/")
+  const projectId = segments[segments.length - 1];
+
   const files = await prisma.file.findMany({
-    where: { projectId: Number(params.projectId) },
+    where: { projectId: Number(projectId) },
     orderBy: { createdAt: 'desc' },
   });
   return NextResponse.json(files);
